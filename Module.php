8da04d4e0516ad931c987e150c538b8e33793467
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-02-23 00:00:00 +0000

    Supported identifiers with percent-encoded slashes.

--- Module.php
+++ Module.php
@@ -24,6 +24,77 @@ class Module extends AbstractModule
 
         $acl = $this->getServiceLocator()->get('Omeka\Acl');
         $acl->allow(null, 'IiifSearch\Controller\Search');
+
+        // Re-encode decoded slashes in identifiers for search routes.
+        $event->getApplication()->getEventManager()
+            ->attach(MvcEvent::EVENT_ROUTE, [$this, 'reencodeIdentifierSlashes'], 1000);
+    }
+
+    /**
+     * Re-encode decoded slashes in IIIF Search URL identifiers.
+     *
+     * @see \IiifServer\Module::reencodeIdentifierSlashes()
+     */
+    public function reencodeIdentifierSlashes(MvcEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (!$request instanceof \Laminas\Http\PhpEnvironment\Request) {
+            return;
+        }
+
+        $path = $request->getUri()->getPath();
+
+        // Quick check: only process IIIF Search paths.
+        if (strpos($path, '/iiif-search/') === false) {
+            return;
+        }
+
+        // Parse: /iiif-search/{identifier}[/search|/list/{name}]
+        if (!preg_match('#^(/iiif-search)(/[^?]*)$#', $path, $matches)) {
+            return;
+        }
+
+        $remainder = substr($matches[2], 1);
+        $segments = explode('/', $remainder);
+        $count = count($segments);
+
+        if ($count <= 1) {
+            return;
+        }
+
+        // Known IIIF Search keywords that appear after the identifier.
+        // @see https://iiif.io/api/search/1.0/
+        // @see https://iiif.io/api/search/2.0/
+        static $searchKeywords = [
+            'search' => true,
+            'autocomplete' => true,
+            'list' => true,
+        ];
+
+        $suffixCount = 0;
+        for ($i = $count - 1; $i >= 1; $i--) {
+            if (isset($searchKeywords[$segments[$i]])) {
+                $suffixCount = $count - $i;
+                break;
+            }
+        }
+
+        $idCount = $count - $suffixCount;
+        if ($idCount <= 1) {
+            return;
+        }
+
+        $idParts = array_slice($segments, 0, $idCount);
+        $suffixParts = array_slice($segments, $idCount);
+        $newRemainder = implode('%2F', $idParts);
+        if ($suffixParts) {
+            $newRemainder .= '/' . implode('/', $suffixParts);
+        }
+
+        $newPath = $matches[1] . '/' . $newRemainder;
+        if ($newPath !== $path) {
+            $request->getUri()->setPath($newPath);
+        }
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager): void
@@ -93,6 +164,14 @@ class Module extends AbstractModule
             ? $plugins->get('iiifCleanIdentifiers')->__invoke($resource->id())
             : $resource->id();
 
+        // When the server does not support encoded slashes, restore literal
+        // slashes in generated URLs so they remain functional.
+        $settings = $this->getServiceLocator()->get('Omeka\Settings');
+        $encodeSlash = (bool) $settings->get('iiifserver_identifier_encode_slash', false);
+        $fixSlash = function (string $url) use ($encodeSlash): string {
+            return $encodeSlash ? $url : strtr($url, ['%252F' => '/', '%2F' => '/']);
+        };
+
         /** @var \IiifServer\Iiif\Manifest $manifest */
         $manifest = $event->getParam('manifest');
 
@@ -102,7 +181,7 @@ class Module extends AbstractModule
         if ($isVersion2) {
             $manifest['service'][] = [
                 '@context' => 'http://iiif.io/api/search/0/context.json',
-                '@id' => $urlHelper('iiifsearch', ['id' => $identifier], ['force_canonical' => true]),
+                '@id' => $fixSlash($urlHelper('iiifsearch', ['id' => $identifier], ['force_canonical' => true])),
                 'profile' => 'http://iiif.io/api/search/0/search',
                 'label' => 'Search within this manifest', // @translate
             ];
@@ -113,14 +192,14 @@ class Module extends AbstractModule
             // old versions of Universal Viewer and used by Wellcome library.
             $service0 = [
                 '@context' => 'http://iiif.io/api/search/0/context.json',
-                '@id' => $urlHelper('iiifsearch', ['id' => $identifier], ['force_canonical' => true]),
+                '@id' => $fixSlash($urlHelper('iiifsearch', ['id' => $identifier], ['force_canonical' => true])),
                 '@type' => 'SearchService0',
                 'profile' => 'http://iiif.io/api/search/0/search',
                 'label' => 'Search within this manifest', // @translate
             ];
             $service1 = [
                 '@context' => 'http://iiif.io/api/search/1/context.json',
-                'id' => $urlHelper('iiifsearch/search', ['id' => $identifier], ['force_canonical' => true]),
+                'id' => $fixSlash($urlHelper('iiifsearch/search', ['id' => $identifier], ['force_canonical' => true])),
                 'type' => 'SearchService1',
                 'profile' => 'http://iiif.io/api/search/1/search',
                 'label' => 'Search within this manifest', // @translate
